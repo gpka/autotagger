0f649a519c622f13afec8fdb1e23f6ca1ca53705
gpaka <sspkpl@stanford.edu>
2015-12-04 05:36:44 -0800

    add spectrum function

--- util.py
+++ util.py
@@ -16,7 +16,10 @@ def stripExtraneous(input):
     return to_return
 
 def getCleanWikiContent(title):
-    return stripExtraneous(wikipedia.page(title).content)
+    return stripExtraneous(wikipedia.page(title).content.lower())
+
+def getWordCountWiki(title):
+    return collections.Counter(getCleanWikiContent(title).split())
 
 def removeMeaningless(text):
     to_remove = {'The', 'the', 'A', 'a', 'is', 'are', 'was', 'were', 'of', 'in',\
@@ -219,3 +222,31 @@ def toSentences(s):
 # get article as sentences (Fairly clean, but still contains weird characters)
 def getWikiSentences(title):
     return toSentences(wikipedia.page(title).content.lower())
+
+# Given two article, return a word spectrum. Input must be a Counter.
+def getWordUniqueness(article1, article2, returntype=collections.Counter):
+    result = returntype()
+    for w in article1:
+        result[w] = 1
+    for w in article2:
+        result[w] = 1
+    for w in result:
+        result[w] = math.log(float(article1[w] + 1) / float(article2[w] + 1))
+    return result
+
+#####################################################################################
+# Compare two article and rank other words in the article onto a spectrum
+# This functions is pretty magical. Can be very useful ! ! !
+
+def getWikiUniqueness(title1, title2, returntype=collections.Counter):
+    return getWordUniqueness(getWordCountWiki(title1), getWordCountWiki(title2), returntype)
+
+def getWikiCompareSpectrum(title1, title2, returntype=collections.Counter):
+    maxi, mini = float('-inf'), float('inf')
+    result = getWikiUniqueness(title1, title2, returntype)
+    for words in result:
+        maxi = max(maxi, result[words])
+        mini = min(mini, result[words])
+    for words in result:
+        result[words] = 2 * float(result[words] - mini) / (maxi - mini) - 1
+    return result
